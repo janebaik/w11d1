594e1abf8e354214e824be9f09f15393992cd099
jane baik <janebaik@berkeley.edu>
2021-01-11 13:26:52 -0800

    setup

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
     const rootEl = document.getElementById('root');
 
-
+    
     ReactDOM.render( <h1>pokedex</h1>, rootEl)
 })
